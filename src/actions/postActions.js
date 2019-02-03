b11f4298484d1f38e7a39fce8dd3af70f3f10de5
Rigved Patki <rigved.patki@gmail.com>
2019-02-03 18:51:49 +0100

    🚧 added redux

---
+++ src/actions/postActions.js
@@ -0,0 +1,26 @@
+import { FETCH_POSTS, NEW_POST } from './types';
+
+export const fetchPosts = () => async dispatch => {
+  const result = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await result.json();
+  dispatch({
+    type: FETCH_POSTS,
+    payload: posts
+  });
+};
+
+export const createPost = postData => async dispatch => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(postData)
+  });
+  const post = await response.json();
+
+  dispatch({
+    type: NEW_POST,
+    payload: post
+  });
+};
